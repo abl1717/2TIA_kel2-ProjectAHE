c69061ca3b0f1bf19ab1a49f1b8768ac7e27a7fe
abl1717 <abil24ti@mahasiswa.pcr.ac.id>
2026-06-07 17:10:34 +0700

    07Juni2026

--- src/data/orangTua.js
+++ src/data/orangTua.js
@@ -1,21 +1,18 @@
 export const orangTuaData = [
   {
     id: 1,
-    userId: 3,
     nama: "Ibu Sari",
     noHp: "081244445555",
     alamat: "Jl. Sakura No. 10 Pekanbaru",
   },
   {
     id: 2,
-    userId: null,
     nama: "Pak Ahmad",
     noHp: "081266667777",
     alamat: "Jl. Dahlia No. 5 Pekanbaru",
   },
   {
     id: 3,
-    userId: null,
     nama: "Ibu Dewi",
     noHp: "082188889999",
     alamat: "Jl. Cempaka No. 7 Pekanbaru",
